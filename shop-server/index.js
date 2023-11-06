8c7a979f91138f7a157cd7fcd26f0e959dec5323
vishal <vishalms2342@gmail.com>
2023-11-07 01:14:29 +0530

    first commit

--- shop-server/index.js
+++ shop-server/index.js
@@ -26,7 +26,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://zealshop.vercel.app",
+		// origin:"http://localhost:3000",
 		credentials:true,
 	})
 )
